c73d6b367d8229bd131ac940db6479b2681888f0
Jeremy Chen <convexset@gmail.com>
2015-12-15 22:14:49 +0800

    Syntax Highlighting theme selection for guide

---
+++ client/header.js
@@ -0,0 +1,23 @@
+/* global ThreeWay: true */
+/* global HighlightJSThemes: true */
+
+ThreeWay.prepare(Template.Header, {
+	viewModelToViewOnly: {
+		theme: HighlightJSThemes.defaultTheme,
+	}
+});
+
+Template.Header.onRendered(function() {
+	var instance = this;
+	instance.autorun(function() {
+		var themeId = instance._3w_.get('theme');
+		HighlightJSThemes.setTheme(themeId);
+	});
+});
+
+Template.Header.helpers({
+	themes: () => _.map(HighlightJSThemes.allThemes, (v, k) => ({
+		id: k,
+		name: v
+	})).sort((x, y) => (x.id > y.id))
+});
