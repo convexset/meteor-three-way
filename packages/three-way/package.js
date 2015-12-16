a70d19ff45aeffa1eaf24e51a6f91c8e819aa328
Jeremy Chen <convexset@gmail.com>
2015-12-17 01:06:42 +0800

    version bump

--- packages/three-way/package.js
+++ packages/three-way/package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'convexset:three-way',
-	version: '0.2.16',
+	version: '0.2.17',
 	summary: 'Flexible and Blaze-friendly three-way data-binding (V-VM-DB) for Meteor',
 	git: 'https://github.com/convexset/meteor-three-way',
 	documentation: '../../README.md'
